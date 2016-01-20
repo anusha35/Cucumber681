efc3386c1c5f3da096ca40386e5fa873547ccd36
anusha35 <sanusha195@gmail.com>
2016-01-20 18:50:29 +0530

    new jenkins

---
+++ cumcumber_jenkins681/src/main/java/cumcumber_jenkins68/cumcumber_jenkins681/App.java
@@ -0,0 +1,13 @@
+package cumcumber_jenkins68.cumcumber_jenkins681;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
